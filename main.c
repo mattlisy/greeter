c6c5936a75978abba351a0152ffad154807b7f36
mattlisy <mattlisy@yahoo.com>
2023-10-30 22:27:24 -0400

    functionality complete must format next

--- main.c
+++ main.c
@@ -3,9 +3,11 @@
 #include <assert.h>
 #include <time.h>
 #include "List.h"
+#include <dirent.h>
+#include <string.h>
 
 #define MAX_QUOTE_LENGTH 1024
-
+#define ART_LINE_LENGTH 255
 /* === Todo ===
 
 - ascii art 
@@ -14,6 +16,37 @@
 */
 
 		
+void print_art() {
+	List art_bank = create_List();
+	DIR* dir = opendir("art/");
+	assert(dir != NULL);
+	struct dirent* file;
+
+	while ((file = readdir(dir)) != NULL) {
+		if (file->d_type == DT_REG) {
+			append_List(&art_bank, file->d_name); 
+		}
+	}
+	
+	closedir(dir);
+
+	char filename[64] = "art/";
+	strcat(filename, get_random_List(art_bank));
+	FILE* art = fopen(filename, "r");
+	assert(art != NULL);
+	char line[ART_LINE_LENGTH];
+	
+		
+	while (fgets(line, ART_LINE_LENGTH, art) != NULL) {	
+		 printf("%s\n", line);
+	}
+	fclose(art);
+
+	
+	
+	
+}
+
 
 
 void grab_quote() {
@@ -32,18 +65,19 @@ void grab_quote() {
 
 
 int main () {
-
-	// hello user
+	
+	print_art();
+	
+	// get user
 	char* user = getenv("USER");
 	assert(user != NULL);
 
 	// time
 	time_t now = time(&now);
 
-	// unique quote
-
-
 	printf("  Hello, %s\n\n\tit is %s\n\n\t", user, ctime(&now));
 	
+	
+	// unique quote
 	grab_quote();
 }
